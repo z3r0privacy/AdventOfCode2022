c9426f2c4ceb18a595e48073b8707f11a34144d5
z3r0privacy <michi_roth@gmx.ch>
2022-12-03 15:07:54 +0100

    added day 03

---
+++ aoc2022/day03.py
@@ -0,0 +1,39 @@
+from day import Day
+
+class Day03 (Day):
+    def __init__(self):
+        super().__init__(__name__)
+        self._isdone = True
+        self._lines = self.read_file_lines()
+
+    def _get_val(self, ch):
+        if ch < ord('a'):
+            return int(ch-ord('A')+27)
+        return int(ch-ord('a')+1)
+
+    def solve_1(self):
+        items = ""
+        for line in self._lines:
+            half = int(len(line)/2)
+            first = line[:half]
+            second = line[half:]
+            for c in first:
+                if c in second:
+                    items += c
+                    break
+        self._get_val(ord(items[0]))
+        return sum([self._get_val(ord(c)) for c in items])
+
+    def solve_2(self):
+        badges = ""
+        pos = 0
+        while True:
+            group = self._lines[pos:pos+3]
+            if len(group) < 3:
+                break
+            for c in group[0]:
+                if c in group[1] and c in group[2]:
+                    badges += c
+                    break
+            pos += 3
+        return sum(self._get_val(ord(c)) for c in badges)
